30f93dfea9f2dc533309ad8ef5519476949b6909
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-10-15 21:24:31 +0600

    'v1.1.2' Data type

--- main.f90
+++ main.f90
@@ -1,6 +1,16 @@
 program name
     implicit none
 
-    print *,'Hello World !'
-    
+    integer :: x = 10
+    real :: y = 5.6
+    complex :: z = (1.0, 2.5)
+    character :: a = 'A'
+    logical :: b = .true.
+
+    print *,x
+    print *,y
+    print *,z
+    print *,a
+    print *,b
+
 end program name
