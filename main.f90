1991061a7893992c28bad06231c2cd7d80fb3594
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-10-16 10:18:39 +0600

    'v1.1.8' array

--- main.f90
+++ main.f90
@@ -1,12 +1,9 @@
-program printNum 
-    implicit none  
-    
-       ! define variables
-       integer :: n = 1
-       
-       do while (n<5) 
-        print *,'Hello World',n
-        n = n+1
-       end do
-       
-    end program printNum  
+program hello
+    implicit none
+
+    integer :: array1(10)  ! 1D integer array of 10 elements
+    array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  ! Array constructor
+    print *, array1(1:10)  ! Print out elements at odd indices
+
+  
+  end program hello
