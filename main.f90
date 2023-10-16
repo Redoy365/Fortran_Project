e4bc4495c1b18a26f1ca894d8e752c30fc7302b3
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-10-16 07:32:11 +0600

    'v1.1.5' if else condition

--- main.f90
+++ main.f90
@@ -1,9 +1,18 @@
-program hello
-
-    read(*,*)x ! user input
-
-!;dflgdf;gdfkg;ldfpoweroweriowe
+program name
+    implicit none
     
-    print *, x
+    integer :: x = 6
+    integer :: y = 6
 
-end program hello
+    if ( x>y ) then ! start condition
+        print *, 'x Grater than y.'
+    
+    else if ( x<y ) then
+        print *, 'x Less then y.'
+    
+    else
+        print *, 'x equal to y.'
+    
+    end if ! end condition
+    
+end program name
